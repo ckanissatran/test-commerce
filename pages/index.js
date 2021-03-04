41ddfd0968ff2d807e115d94995057b5775e246d
Anissa Tran <ckanissatran@gmail.com>
2021-03-05 03:04:22 +0430

    Test script loading method

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,50 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import PayPalButton from '../components/PayPalButton'
+import React from 'react'
+
 
 export default function Home() {
+  const [sdkReady, setSdkReady] = React.useState(false);
+
+  const addPaypalSdk = () => {
+    console.log("&&&&HEY", process.env.CLIENT_ID)
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.CLIENT_ID}`;
+    script.async = true;
+    script.onload = () => {
+      setSdkReady(true);
+    };
+    script.onerror = (err) => {
+      console.log(err)
+      throw new Error('Paypal SDK could not be loaded.');
+    };
+
+    document.body.appendChild(script);
+  };
+
+  React.useEffect(() => {
+    if (window !== undefined && window.paypal === undefined) {
+      addPaypalSdk();
+    }
+    return () => {
+      document.body.removeChild(script);
+    }  
+  }, [])
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.CLIENT_ID}`}></script>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <PayPalButton />
+        {sdkReady ? <PayPalButton /> : <h1>Loading...</h1>}
       </main>
 
     </div>
--- pages/programs.js
+++ pages/programs.js
@@ -1,14 +1,39 @@
 import React from 'react'
-import Head from 'next/head'
 import SubscribeButton from '../components/SubscribeButton'
+import ReactDOM from 'react-dom';
 
 export default function Programs() {
+    const [sdkReady, setSdkReady] = React.useState(false);
+
+    const addPaypalSdk = () => {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.CLIENT_ID}&vault=true&intent=subscription`;
+      script.async = true;
+    //   script.dataSdkIntegrationSource = "button-factory";
+      script.onload = () => {
+        setSdkReady(true);
+      };
+      script.onerror = () => {
+        throw new Error('Paypal SDK could not be loaded.');
+      };
+  
+      document.body.appendChild(script);
+    };
+  
+    React.useEffect(() => {
+      if (window !== undefined && window.paypal === undefined) {
+        addPaypalSdk();
+      }
+      return () => {
+        document.body.removeChild(script);
+      }      
+    }, [])
+
     return (<>
-        <Head>
-            <script src={`https://www.paypal.com/sdk/js?client-id=${process.env.CLIENT_ID}&vault=true&intent=subscription`} data-sdk-integration-source="button-factory"></script>
-        </Head>
+        
         <div>Hello you are on the programs page</div>
-        <SubscribeButton/>
+        {sdkReady ? <SubscribeButton/> : <h1>Loading...</h1>}
 
     </>
     )
