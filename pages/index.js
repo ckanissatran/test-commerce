8579d1d74d83a2b864346d61d8c44c327ef02637
Anissa Tran <ckanissatran@gmail.com>
2021-03-08 03:39:53 +0430

    Hide paypal sandbox

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,26 @@ import PayPalButton from '../components/PayPalButton'
 import React from 'react'
 
 export default function Home() {
-  const [sdkReady, setSdkReady] = React.useState(false);
+  // const [sdkReady, setSdkReady] = React.useState(false);
 
-  React.useEffect(() => {
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_CLIENT_ID}`;
-    script.async = true;
-    script.onload = () => {
-      setSdkReady(true);
-    };
-    script.onerror = (err) => {
-      console.log(err)
-      throw new Error('Paypal SDK could not be loaded.');
-    };
-    document.body.appendChild(script);
+  // React.useEffect(() => {
+  //   const script = document.createElement('script');
+  //   script.type = 'text/javascript';
+  //   script.src = `https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_CLIENT_ID}`;
+  //   script.async = true;
+  //   script.onload = () => {
+  //     setSdkReady(true);
+  //   };
+  //   script.onerror = (err) => {
+  //     console.log(err)
+  //     throw new Error('Paypal SDK could not be loaded.');
+  //   };
+  //   document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    }  
-  }, [])
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   }  
+  // }, [])
 
   return (
     <div className={styles.container}>
@@ -36,7 +36,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        {sdkReady ? <PayPalButton /> : <h1>Loading...</h1>}
+        {/* {sdkReady ? <PayPalButton /> : <h1>Loading...</h1>} */}
       </main>
 
     </div>
