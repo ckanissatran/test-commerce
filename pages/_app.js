dea40ed9d68a1c476fb7c7cbdebf86fa21c2b644
Anissa Tran <ckanissatran@gmail.com>
2021-03-08 19:55:52 +0430

    Update env vars

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-import { firebaseConfig } from '../keys'
 import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+console.log(process.env.NODE_ENV)
 
 if(firebase.apps.length === 0){
   firebase.initializeApp(firebaseConfig)
@@ -13,3 +23,5 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+
